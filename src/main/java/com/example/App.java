41c1bdbf7364acc38b4aede71cbb615b2fad3905
bontha-rajesh <138701140+bontha-rajesh@users.noreply.github.com>
2025-04-07 17:46:09 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -17,7 +17,7 @@ public class App {
     @GetMapping("/")
     public String home(Model model) {
         // Pass the header text to the view
-        model.addAttribute("header", "Cloud Computing in Telugu-AWS devops");
+        model.addAttribute("header", "Cloud Computing in Telugu-AWS devops in telugu");
         return "index"; // Return the index.html template
     }
 }
